802c1b8547e1cfb39d9ab0c5c21edfa527170fe5
SaladFrog <48181364+SaladFrog@users.noreply.github.com>
2021-01-21 17:58:32 +0100

    Update fn.Vector.js

--- fn.Vector.js
+++ fn.Vector.js
@@ -4,8 +4,16 @@ function Vector(x, y, z) {
   this.z = z || 0;
 }
 
+// alt 1
+Vector.prototype.copy = function () {
+  return new Vector(this.x, this.y, this.x)
+}
+// Vector.prototype.other = function () {}
+
+// alt 2
 Vector.prototype = {
   copy: function () {
     return new Vector(this.x, this.y, this.z);
   },
+  // other: function () {}
 };
